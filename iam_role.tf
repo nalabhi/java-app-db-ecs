af9c090dabe4f29d2b8d29a60fb86bae588168d5
nalabhi <100408534+nalabhi@users.noreply.github.com>
2022-05-13 13:55:06 +0530

    Update iam_role.tf

--- iam_role.tf
+++ iam_role.tf
@@ -1,5 +1,5 @@
 ## ECS task execution role data
-data "aws_iam_policy_document" "ecs_task_execution_role" {
+data "aws_iam_policy_document" "ecs_task_execution_role_new" {
   version = "2012-10-17"
   statement {
     sid     = ""
@@ -14,13 +14,13 @@ data "aws_iam_policy_document" "ecs_task_execution_role" {
 }
 
 # ECS task execution role
-resource "aws_iam_role" "ecs_task_execution_role" {
+resource "aws_iam_role" "ecs_task_execution_role_new" {
   name               = var.ecs_task_execution_role
-  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_role.json
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_role_new.json
 }
 
 # ECS task execution role policy attachment
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_new" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
